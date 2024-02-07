999aff8f1e3b2de945e40a120f23f120a31e526e
Riley Xu <riley.xu@gmail.com>
2024-02-07 18:25:20 -0500

    docstring

--- variable.py
+++ variable.py
@@ -5,19 +5,29 @@
 @brief Variable naming utility class 
 '''
 class Variable:
-    def __init__(self, name, xtitle, unit):
+    '''
+    @property name
+        The base name that uniquely identifies the variable. This should match the 
+        histogram naming conventions.
+    @property title
+        A TLatex version of the variable that is used in title text.
+    @property unit
+        The name of the unit of the variable. Again this should match the histogram 
+        outputs.
+    '''
+    def __init__(self, name, title, unit):
         self.name = name
-        self.xtitle = xtitle
+        self.title = title
         self.unit = unit
     
     def __format__(self, __format_spec: str) -> str:
         if __format_spec:
             if self.unit:
-                return f'{self.xtitle} [{self.unit}]'
+                return f'{self.title} [{self.unit}]'
             else:
-                return f'{self.xtitle}'
+                return f'{self.title}'
         else:
             return self.name
     
-vv_m = Variable(name="vv_m", xtitle="m(VV)", unit="GeV")
+vv_m = Variable(name="vv_m", title="m(VV)", unit="GeV")
 
