29d0b3b3c1a0d06faf515983e4a1a8ffa1dd4334
Riley Xu <riley.xu@gmail.com>
2024-04-23 19:37:36 -0500

    update get_systs to use a single file

--- get_systs.py
+++ get_systs.py
@@ -9,15 +9,8 @@
 RUN
 ------------------------------------------------------------------------------------------
 
-    get_systs.py filepath/formatter_1.root [...] 
+    get_systs.py hist_file.root
 
-This will fetch histogram files using the naming convention supplied in the arguments.
-These arguments can include python formatters (using curly braces) for 'lep' and 'sample'.
-For example,
-
-    hists/{lep}_{sample}.root
-    
-See [utils.FileManager] for details.
 '''
 
 import ROOT # type: ignore
@@ -27,17 +20,19 @@ from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 import utils
 
 
-def get_systs(file_manager : utils.FileManager):
-    f_template = file_manager.files[(1, utils.Sample.diboson.name)][0]
-    start_key = 'SMVV_VV1Lep_MergHP_Inclusive_SR_lvJ_m_'
+def get_systs(filepath):
+    f_template = ROOT.TFile(filepath)
+    name_stub = '_MergHP_Inclusive_SR_'
     end_key = '__1up'
 
-    systs = []
+    systs = set()
     for key in f_template.GetListOfKeys():
         name = key.GetName()
-        if name.startswith(start_key) and name.endswith(end_key):
-            systs.append(name[len(start_key):-len(end_key)])
+        if name_stub in name and name.endswith(end_key):
+            i = name.index('_Sys')
+            systs.add(name[i+1:-len(end_key)])
 
+    systs = list(systs)
     systs.sort()
     return systs
 
@@ -54,19 +49,9 @@ def parse_args():
     parser.add_argument('filepaths', nargs='+')
     return parser.parse_args()
 
-
-def get_files(filepaths):
-    file_manager = utils.FileManager(
-        samples=[utils.Sample.diboson],
-        file_path_formats=filepaths,
-    )
-    return file_manager
-
-
 def main():
     args = parse_args()
-    file_manager = get_files(args.filepaths)
-    systs = get_systs(file_manager)
+    systs = get_systs(args.filepaths[0])
 
     for x in systs:
         print(f"'{x}',")
